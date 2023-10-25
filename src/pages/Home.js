c76ef381e2e719361c94bcdd2421beb00425fffd
Eduard Buna <eduardbuna@yahoo.com>
2023-10-25 15:02:14 -0400

    Creating basic react app using old home page

---
+++ src/pages/Home.js
@@ -0,0 +1,31 @@
+import './Home.css';
+import React from "react"
+import { Link } from 'react-router-dom';
+
+function Home() {
+    return (
+        <div className='homePageWrapper'>
+            <div className="welcomeText">
+                <div>Welcome!</div>
+            </div>
+            <div className="sectionText">
+                <div>View Projects and Items</div>
+            </div>
+            <div className='navWrapper'>
+                <Link to='/'>
+                    <div className='navButtonWrapper'>
+                        All Projects
+                    </div>
+                </Link>
+                <Link to='/'>
+                    <div className='navButtonWrapper'>
+                        All Items
+                    </div>
+                </Link>
+            </div>
+            <div style={{'height': '70px', 'width': '100%'}}></div>
+        </div>
+    )
+}
+
+export default Home
